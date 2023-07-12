b0a3189164c446817aced3b8ea4d08e3ced73d7a
Barczi Bálint <balint.barczi@supercharge.io>
2023-07-12 13:54:23 +0200

    chore: initial commit 4

--- Package.swift
+++ Package.swift
@@ -15,12 +15,12 @@ let package = Package(
     .binaryTarget(
         name: "Realm",
         url: "https://github.com/barcister/RealmBineries/releases/download/v10.41.0/Realm.xcframework.zip",
-        checksum: "f21f536612912a43b0c0d3e0159efac1cb5dbc8012a3d00de73c59b5bd388bfd" // Realm
+        checksum: "6da556a1536bb553ad9c4d5cf8331bd1c531f8338a796a1bede708f58e863b14" // Realm
     ),
     .binaryTarget(
         name: "RealmSwift",
         url: "https://github.com/barcister/RealmBineries/releases/download/v10.41.0/RealmSwift.xcframework.zip",
-        checksum: "e08327525cf1a44da6a7805b19c981f383c4cb2495bdeeca7b26bcfaffd98076" // RealmSwift
+        checksum: "94a1dfdc59a8d3fa1cee7d47b0873bfd29c39ea96571df5247dd7f26e89d31e4" // RealmSwift
     )
   ]
 )
