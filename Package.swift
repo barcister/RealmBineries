3a11655dc4aab98bb43995bc825b55ac9237f273
Barczi Bálint <balint.barczi@supercharge.io>
2023-07-26 00:17:00 +0100

    chore: update to xcode 15 beta 5

--- Package.swift
+++ Package.swift
@@ -15,12 +15,12 @@ let package = Package(
     .binaryTarget(
         name: "Realm",
         url: "https://github.com/barcister/RealmBineries/releases/download/v10.41.0/Realm.xcframework.zip",
-        checksum: "6da556a1536bb553ad9c4d5cf8331bd1c531f8338a796a1bede708f58e863b14" // Realm
+        checksum: "a13cf785ac0f067ca23004947b5677db04733e94834211ce2aa6df7c266b61a3" // Realm
     ),
     .binaryTarget(
         name: "RealmSwift",
         url: "https://github.com/barcister/RealmBineries/releases/download/v10.41.0/RealmSwift.xcframework.zip",
-        checksum: "94a1dfdc59a8d3fa1cee7d47b0873bfd29c39ea96571df5247dd7f26e89d31e4" // RealmSwift
+        checksum: "15d8190831df41042991ce38c2bef29187d05d585076c83f089a5e856448ac76" // RealmSwift
     )
   ]
 )
