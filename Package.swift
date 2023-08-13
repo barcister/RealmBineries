355674e9e9aa018e1631da32864e753e23b31bcd
Barczi Bálint <balint.barczi@supercharge.io>
2023-08-14 00:23:06 +0100

    chore: update version to 10.42.2

--- Package.swift
+++ Package.swift
@@ -14,12 +14,12 @@ let package = Package(
   targets: [
     .binaryTarget(
         name: "Realm",
-        url: "https://github.com/barcister/RealmBineries/releases/download/v10.41.0/Realm.xcframework.zip",
+        url: "https://github.com/barcister/RealmBineries/releases/download/v10.42.2/Realm.xcframework.zip",
         checksum: "a13cf785ac0f067ca23004947b5677db04733e94834211ce2aa6df7c266b61a3" // Realm
     ),
     .binaryTarget(
         name: "RealmSwift",
-        url: "https://github.com/barcister/RealmBineries/releases/download/v10.41.0/RealmSwift.xcframework.zip",
+        url: "https://github.com/barcister/RealmBineries/releases/download/v10.42.2/RealmSwift.xcframework.zip",
         checksum: "15d8190831df41042991ce38c2bef29187d05d585076c83f089a5e856448ac76" // RealmSwift
     )
   ]
